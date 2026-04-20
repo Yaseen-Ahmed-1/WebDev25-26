fff2c3909ee590255bfdc6aab1c0c62fde65cd82
Yaseen-Ahmed-1 <Yaseena18@nycstudents.net>
2026-04-20 17:52:16 +0000

    aa

---
+++ WDLesson6_4Practical/script.js
@@ -0,0 +1,42 @@
+let data, info; //global variables
+
+async function init(){
+  // Challenge 1: Retrieve the FBI data from https://raw.githubusercontent.com/rcastro2/WebDevelopment/refs/heads/main/data/fbi.json
+  let link = "https://raw.githubusercontent.com/rcastro2/WebDevelopment/refs/heads/main/data/fbi.json";
+  info = await fetch(link);
+  data = await info.json();
+  
+  let output = document.getElementById("output");
+  let build = "";
+  /* Challenge 2: 
+          1) Traverse the data using a 'for' loop.  
+          2) Create a variable to extract each criminal's information from the dataset.
+          3) Using the variable created, generate HTML to display the information for each criminal.
+
+     Note: To view the PDF of the criminal poster include the following code before the string interpolated url
+     into a hyperlink in order to actually display the PDF in a new tab.
+     https://mozilla.github.io/pdf.js/web/viewer.html?file=${...}
+  */
+ for (let i = 0; i < data.length; i++) {
+    let criminal = data[i]; // extract each record
+
+    build += `
+      <div class="card">
+        <h2>${criminal.title}</h2>
+        <img src="${criminal.image}" alt="${criminal.title}" width="200">
+        <p><strong>Sex:</strong> ${criminal.sex}</p>
+        <p>${criminal.details}</p>
+        <p>
+          <a href="https://mozilla.github.io/pdf.js/web/viewer.html?file=${criminal.pdf}" target="_blank">
+            View Poster (PDF)
+          </a>
+        </p>
+        <hr>
+      </div>
+    `;
+  }
+
+  output.innerHTML = build;
+}
+
+
